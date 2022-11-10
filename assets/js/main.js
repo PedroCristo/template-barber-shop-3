76a2e594cf28f84145c511513c3a23313a17f624
Pedro Cristo <pcristo35@gmail.com>
2022-11-10 14:34:38 +0000

    Add contact form and footer

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+// Gets full year and display on the footer
+var date = new Date();
+
+var year = date.getFullYear();
+var month = date.getMonth() + 1
+var time = date.getHours()
+
+if (time >= 14) {
+    var day = date.getDate() + 1
+} else {
+    var day = date.getDate()
+}
+
+var fullDate = year + "-" + month + "-" + day;
+
+document.getElementById("outputYear").innerHTML =  year
+document.getElementById("inputDate").min = fullDate
+
+// Smooth scrolling to all links
+$("a").on('click', function (e) {
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      e.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800);
+    }
+    // End if
+  });
